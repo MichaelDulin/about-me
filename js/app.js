84a2d443e5810ce0143e732b3265f99996e6f803
Michael Dulin <michaeldulin1211@gmail.com>
2023-01-27 11:30:51 -0800

    Finished question 6/7 - requires further testing

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ let userResponse2;
 let userResponse3;
 let userResponse4;
 let userResponse5;
-
+let guessesLeft = 4;
+let userPoints = 0;
+let userResonse7;
 
 getUserName();
-q1(); q2(); q3(); q4(); q5();
+q1(); q2(); q3(); q4(); q5(); q6(); q7();
 
 function getUserName() {
   // Get user name
@@ -20,15 +22,21 @@ function getUserName() {
   alert('Hello, ' + userName + ' and welcome to my portfolio!');
 }
 
+function calcPoints() {
+  alert('Total Points: ' + userPoints);
+}
+
 // Question 1:
 function q1() {
   userResponse1 = prompt('Have I ever lived in another Country?');
   if (userResponse1 === 'yes' || userResponse1 === 'y') {
     alert('Good job, ' + userName + ' That is correct!');
+    userPoints += 1;
   } else {
     //console.log('Sorry, ' + userName + '. That is not correct.');
     alert('Sorry, ' + userName + '. That is not correct.');
   }
+  calcPoints();
 }
 
 // Question 2:
@@ -37,32 +45,40 @@ function q2() {
   userResponse2 = prompt('Do I coach baseball?');
   if (userResponse2 === 'yes' || userResponse2 === 'y') {
     alert('Good job, ' + userName + ' That is correct!');
+    userPoints += 1;
   } else {
     //console.log('Sorry, ' + userName + '. That is not correct.');
     alert('Sorry, ' + userName + '. That is not correct.');
   }
+  calcPoints();
 }
+
 // Question 3:
 function q3() {
 
   userResponse3 = prompt('Was I in the military?');
   if (userResponse3 === 'yes' || userResponse3 === 'y') {
     alert('Good job, ' + userName + ' That is correct!');
+    userPoints += 1;
   } else {
     //console.log('Sorry, ' + userName + '. That is not correct.');
     alert('Sorry, ' + userName + '. That is not correct.');
   }
+  calcPoints();
 }
+
 // Question 4:
 function q4() {
 
   userResponse4 = prompt('Have I ever lived in Canada?');
   if (userResponse4 === 'no' || userResponse4 === 'n') {
     alert('Good job, ' + userName + ' That is correct!');
+    userPoints += 1;
   } else {
     ///console.log('Sorry, ' + userName + '. That is not correct.');
     alert('Sorry, ' + userName + '. That is not correct.');
   }
+  calcPoints();
 }
 // Question 5:
 function q5() {
@@ -70,9 +86,62 @@ function q5() {
   userResponse5 = prompt('Do I like programming?');
   if (userResponse5 === 'yes' || userResponse5 === 'y') {
     alert('Good job, ' + userName + ' That is correct!');
+    userPoints += 1;
   } else {
     //console.log('Sorry, ' + userName + '. That is not correct.');
     alert('Sorry, ' + userName + '. That is not correct.');
   }
+  calcPoints();
+}
+
+// Crashing on loop breakout
+function q6() {
+  let correctNum = 7;
+  let condition = 0;
+  let userNum = prompt('Guess a number between 1 and 10:');
+  while (condition !== 1) {
+    if (+userNum < correctNum) {
+      guessesLeft -= 1;
+      userNum = prompt('That is too low! Guesses remaining = '
+      + guessesLeft + '. Guess a number beteween 1 and 10:');
+    } else if (+userNum > correctNum) {
+      guessesLeft -= 1;
+      userNum = prompt('That is too high! Guesses remaining = '
+      + guessesLeft + '. Guess a number beteween 1 and 10:');
+    }
+    if (+userNum === correctNum) {
+      userPoints += 1;
+      condition = 1;
+      alert('That is correct!');
+    }
+    if (guessesLeft <= 0) {
+      condition = 1;
+      alert('You ran out of chances!');
+    }
+  }
+  calcPoints();
+}
+
+// Untested
+function q7() {
+  guessesLeft = 6;
+  let correctGuesses = 0;
+  let myArr = ['Seattle', 'Yokosuka', 'Norfolk', 'Jacksonville'];
+  while (guessesLeft > 0 && correctGuesses < 4){
+    userResonse7 = prompt('What 4 cities have I lived in? (guesses reamining = ' + guessesLeft + '): ');
+    for (let i = 0; i < myArr.length; i++) {
+      if (userResonse7 === myArr[i]) {
+        alert('That is a correct answer! I have lived in ' + userResonse7);
+        correctGuesses += 1;
+        guessesLeft -= 1;
+        userPoints += 1;
+      } else {
+        alert('That is not correct. Try again!');
+        guessesLeft -= 1;
+      }
+    }
+  }
+  alert('Here all all the places I have lived: ' + myArr);
+  alert('Thanks for playing, ' + userName + '! Your final score: ' + userPoints);
 }
 
